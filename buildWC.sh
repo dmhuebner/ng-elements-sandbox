bfeb12b3632a7469dbf20fa2920457fe404e2d50
David <davidmh.mail@gmail.com>
2019-08-15 11:10:34 -0600

    Change WC build to bundle styles separately

--- buildWC.sh
+++ buildWC.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ng build ng-elements-sandbox --prod --output-hashing=none --single-bundle --keep-polyfills
+ng build ng-elements-sandbox --prod --output-hashing=none --single-bundle --keep-polyfills --bundle-styles false
 mv dist/ng-elements-sandbox/main-es2015.js demo/ngelements.js
 mv dist/ng-elements-sandbox/polyfills-es2015.js demo
+mv dist/ng-elements-sandbox/styles.css demo
 
